0cb2088d38d149152036a492ebf5fe3c16e682fa
wushaoch <shao.wu@hotmail.com>
2017-03-30 15:26:44 -0400

    figured out GeoJSON
    now we can manually enter data points

--- sample_geojson.js
+++ sample_geojson.js
@@ -1,33 +1,31 @@
-{ "type": "FeatureCollection",
-    "features": [
-      { "type": "Feature",
-        "geometry": {"type": "Point", "coordinates": [102.0, 0.5]},
-        "properties": {"prop0": "value0"}
-        },
-      { "type": "Feature",
-        "geometry": {
-          "type": "LineString",
-          "coordinates": [
-            [102.0, 0.0], [103.0, 1.0], [104.0, 0.0], [105.0, 1.0]
-            ]
-          },
-        "properties": {
-          "prop0": "value0",
-          "prop1": 0.0
-          }
-        },
-      { "type": "Feature",
-         "geometry": {
-           "type": "Polygon",
-           "coordinates": [
-             [ [100.0, 0.0], [101.0, 0.0], [101.0, 1.0],
-               [100.0, 1.0], [100.0, 0.0] ]
-             ]
-         },
-         "properties": {
-           "prop0": "value0",
-           "prop1": {"this": "that"}
-           }
-         }
-       ]
-     }
+eqfeed_callback( {
+    "type":"FeatureCollection", 
+    "metadata": {
+        "generated": 1490900696000, 
+        "url": "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp", 
+        "title": "USGS Magnitude 2.5+ Earthquakes, Past Week", 
+        "status": 200, 
+        "api": "1.5.7", 
+        "count": 255
+           },
+    "features":
+    [ 
+      // insert entries
+      {
+        "type":"Feature", "properties": {
+            "mag": 100.1, "place": "112km SW of San Pedro de Atacama, Chile", "time": 1490297547000, "updated": 1490305809040, "tz": -240, "url": "https://earthquake.usgs.gov/earthquakes/eventpage/us20008u42", "detail": "https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/us20008u42.geojsonp", "felt": null, "cdi": null, "mmi": null, "alert": null, "status": "reviewed", "tsunami": 0, "sig": 259, "net": "us", "code": "20008u42", "ids": ",us20008u42,", "sources": ",us,", "types": ",geoserve,origin,phase-data,", "nst": null, "dmin": null, "rms": 0.88, "gap": null, "magType": "mb", "type": "earthquake", "title": "M 4.1 - 112km SW of San Pedro de Atacama, Chile"
+        }
+        , "geometry": {
+            "type": "Point", "coordinates": [-69.118, -23.503, 105.3]
+        }
+        , "id":"us20008u42"
+      }
+
+
+
+
+    ] //end of "features"
+      ,  "bbox":[-179.6897, -60.5845, 0, 179.8707, 69.6244, 556.1]
+  }
+
+);
