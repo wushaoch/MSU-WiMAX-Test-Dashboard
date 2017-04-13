924185efb036c1098cb0683c6ffcc5f59585717f
wushaoch <shao.wu@hotmail.com>
2017-04-13 16:47:05 -0400

    detail box now display data correctly

--- assets/map_setup.js
+++ assets/map_setup.js
@@ -32,23 +32,31 @@ function initMap() {
     document.getElementsByTagName('head')[0].appendChild(script);
 
     map.data.setStyle(function(feature) {
-        var mag = feature.getProperty('received_throughput');
+        var mag_normalized = normalizeMag(
+                                    feature.getProperty('received_throughput'),
+                                    100,
+                                    0
+                                    );
         return {
-            icon: getCircle(mag)
+            icon: getCircle(mag_normalized)
         };
     });
     
 
     map.data.addListener('click', function(event) {
-        document.getElementById("db_header_text").innerHTML="Data Point #?";
-        document.getElementById("db_value_1").innerHTML=event.feature.getProperty('throughput');
-        document.getElementById("db_value_2").innerHTML=event.feature.getProperty('throughput');
-        document.getElementById("db_value_3").innerHTML=event.feature.getProperty('throughput');
+        document.getElementById("db_header_text").innerHTML="Data Point";
+        document.getElementById("receive_value").innerHTML=event.feature.getProperty('received_throughput');
+        document.getElementById("send_value").innerHTML=event.feature.getProperty('sent_throughput');
+        document.getElementById("db_value_3").innerHTML=event.feature.getProperty('base_station_signal_strength');
+        document.getElementById("db_value_4").innerHTML=event.feature.getProperty('subscriber_unit_signal_strength');
+        document.getElementById("db_value_5").innerHTML=event.feature.getProperty('base_station_SNR');
+        document.getElementById("db_value_6").innerHTML=event.feature.getProperty('subscriber_unit_SNR');
+        document.getElementById("detail_box").style.display = "inline";
     });
 
     map.data.addListener('mouseover', function(event) {
         map.data.revertStyle();
-        map.data.overrideStyle(event.feature, {icon: getCircle(0)});
+        map.data.overrideStyle(event.feature, {icon: getCircle(-2)});
     });
 
     map.data.addListener('mouseout', function(event) {
@@ -56,8 +64,8 @@ function initMap() {
     });
 }
 
-function getCircle(throughput) {
-    var color = getColor(throughput / 10 * 100);
+function getCircle(mag_normalized) {
+    var color = getColor(mag_normalized);
     return {
         path: google.maps.SymbolPath.CIRCLE,
         fillColor: color, // need to calculate color according to strength
@@ -69,9 +77,29 @@ function getCircle(throughput) {
     };
 }
 
+function normalizeMag(mag, max, min) { // normalize to 0-100, 
+    if (mag == 999) // connection failure
+    {
+        return -1;
+    }
+    else 
+    {
+        return 100*(mag-min)/(max-min);
+    }
+}
+
 function getColor(mag) {
     // intensity on a scale range from 0 to 100
-    
+    if (mag == -1) 
+    {
+        console.log("changed to black");
+        return "#050505"; // return black
+    }
+    if (mag == -2)
+    {
+        return "#00FF00";
+    }
+
     var top_r = 31;  //#1FF8FF
     var top_g = 248;
     var top_b = 255;
@@ -79,16 +107,16 @@ function getColor(mag) {
     var btm_g = 0;
     var btm_b = 0;
 
-    // color only starts to change at 70% height of the bar
-    if (mag >= 70)
+    // color only starts to change at 90% height of the bar
+    if (mag >= 100)
     {
         return "#1FF8FF";
     }
     else
     {
-        var r = Math.round(btm_r + (mag * (top_r - btm_r)) / 70);
-        var g = Math.round(btm_g + (mag * (top_g - btm_g)) / 70);
-        var b = Math.round(btm_b + (mag * (top_b - btm_b)) / 70); 
+        var r = Math.round(btm_r + (mag * (top_r - btm_r)) / 100);
+        var g = Math.round(btm_g + (mag * (top_g - btm_g)) / 100);
+        var b = Math.round(btm_b + (mag * (top_b - btm_b)) / 100); 
         return RgbDecToHex(r, g, b);
     } 
 }
@@ -108,4 +136,8 @@ function RgbDecToHex(r_d, g_d, b_d) {
 
 function datafeed_callback(results) {
     map.data.addGeoJson(results);
+}
+
+function close_detail_box(){
+    document.getElementById("detail_box").style.display = "none";
 }
