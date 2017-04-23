ef31c65770a8dd8af0d01b928e84c4dd46827a85
stharo <tharosoun@gmail.com>
2017-04-23 14:57:45 -0400

    linechart adjusted

--- linechart/linechart.js
+++ linechart/linechart.js
@@ -1,10 +1,10 @@
 google.charts.load('current', {'packages':['line','corechart']});
-    google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawChart);
 
-    function drawChart() {
-        
+function drawChart() {
+    
     var databaseread = $.ajax({
-          url: "getdata.php",
+          url: "./linechart/getdata.php",
           dataType: "json",
           async: false
           }).responseText;
@@ -17,27 +17,27 @@ google.charts.load('current', {'packages':['line','corechart']});
     for (var i = 1; i < jsonData.length; i++ ){
         jsonData[i][0] = new Date(jsonData[i][0]);
     }
-
-        var data = new google.visualization.arrayToDataTable(jsonData);
     
-        var options = {
-          title: 'Well Status',
-      curveType: 'function',
-      series: {
-        0: {targetAxisIndex: 1},
-        1: {targerAxisIndex: 2}
-      },
-      hAxis: {
-        format: 'M/d/yy hh:mm:ss.SSS',
-                title: 'Time'
-          },
-          vAxes: {0:{title: 'Voltage (V)'}, 1:{title:'Pump Flow Speed (Gallons/mn)'}},
-            vAxis: {viewWindow: { max: 20 }},
-          legend: { position: 'bottom' }
-        };
+    var data = new google.visualization.arrayToDataTable(jsonData);
 
-        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+    var options = {
+        title: 'Well Status',
+        curveType: 'function',
+        series: {
+            0: {targetAxisIndex: 1},
+            1: {targerAxisIndex: 2}
+        },
+        hAxis: {
+            format: 'M/d/yy hh:mm:ss.SSS',
+            title: 'Time'
+        },
+        vAxes: {0:{title: 'Voltage (V)'}, 1:{title:'Pump Flow Speed (Gallons/mn)'}},
+        vAxis: {viewWindow: { max: 20 }},
+        legend: { position: 'bottom' }
+    };
 
-        chart.draw(data, options);
-      }
-    setInterval(function(){drawChart();}, 400);
+    var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+    chart.draw(data, options);
+}
+setInterval(function(){drawChart();}, 400);
